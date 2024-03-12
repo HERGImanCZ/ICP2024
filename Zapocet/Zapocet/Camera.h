f45aae43d12cbba693c90b9ad3ea662da66284e9
Jan Hergesel <j.hergesel@seznam.cz>
2024-03-12 16:27:07 +0100

    first commit

---
+++ Zapocet/Zapocet/Camera.h
@@ -0,0 +1,40 @@
+#ifndef CAMERA_CLASS_H
+#define CAMERA_CLASS_H
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/rotate_vector.hpp>
+#include <glm/gtx/vector_angle.hpp>
+
+#include "shaderClass.h"
+
+class Camera {
+public:
+	glm::vec3 Position;
+	glm::vec3 Orientation = glm::vec3(0.0f, 0.0f, -1.0f);
+	glm::vec3 Up = glm::vec3(0.0f, 1.0f, 0.0f);
+	glm::mat4 cameraMatrix = glm::mat4(1.0f);
+
+	int width;
+	int height;
+	bool firstClick = true;
+
+	float speed = 0.0005f;
+	float sensitivity = 100.0f;
+
+	Camera(int width, int height, glm::vec3 position);
+
+	// Updates the camera matrix to the Vertex Shader
+	void UpdateMatrix(float FOVdeg, float nearPlane, float farPlane);
+	// Exports the camera matrix to a shader
+	void Matrix(Shader& shader, const char* uniform);
+	void Inputs(GLFWwindow* window);
+
+};
+
+#endif
